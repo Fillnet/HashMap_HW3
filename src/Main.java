de3d15997ff5d01ff73c6d80e70fef8872bd76c7
Fillnet <filipp@tenikaytis.ru>
2022-11-30 23:20:13 +0300

    HW3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String, Integer> map = new HashMap<>();
+        map.put("str1", 2);
+        map.put("str2", 1);
+        map.put("str1", 2);
+        map.put("str1", 5);
+        System.out.println(map);
+//        map.addToMap("str1", 2);
     }
+        
 }
